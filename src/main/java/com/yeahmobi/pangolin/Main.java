b3a13b0ad6f393983115bfa3610c43e98e17a7a0
Li Zhanhui <lizhanhui@gmail.com>
2016-10-11 09:58:14 +0800

    Add port binding

--- src/main/java/com/yeahmobi/pangolin/Main.java
+++ src/main/java/com/yeahmobi/pangolin/Main.java
@@ -1,8 +1,21 @@
 package com.yeahmobi.pangolin;
 
+import org.apache.commons.cli.*;
+
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws ParseException {
+        Options options = new Options();
+        options.addOption("p", "port", true, "port to bind");
+        CommandLineParser commandLineParser = new DefaultParser();
+        CommandLine commandLine = commandLineParser.parse(options, args);
+
+        if (!commandLine.hasOption("p")) {
+            System.exit(1);
+        }
+
         ServerConfig config = new ServerConfig();
+        config.setPort(Integer.parseInt(commandLine.getOptionValue('p')));
+
         Server server = new Server(config);
         server.start();
     }
